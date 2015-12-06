2608945cbe4c5142e1d0c03c2c5e968aa1357363
Jason Green <greener@Ari-Gold.local>
2015-12-06 09:07:08 +0000

    only download mc if it doesn't exsist

--- minecraft-start.sh
+++ minecraft-start.sh
@@ -5,7 +5,10 @@ VERSION=$(cat /minecraft/host/config/mcversion)
 
 cd /minecraft/host/mcdata
 
-wget -t inf https://s3.amazonaws.com/Minecraft.Download/versions/$VERSION/minecraft_server.$VERSION.jar
+if [[ ! -f minecraft_server.$VERSION.jar ]]; then
+
+	wget -t inf https://s3.amazonaws.com/Minecraft.Download/versions/$VERSION/minecraft_server.$VERSION.jar
+fi
 
 java -jar minecraft_server.$VERSION.jar nogui
 
