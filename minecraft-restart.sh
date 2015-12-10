b86c54c5b1125dac3772bc8a4e55933db9b340bc
Jason Green <greener@Ari-Gold.local>
2015-12-10 16:59:36 +0000

    changes to minecraft startup

--- minecraft-restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-/minecraft/vanillabean.py "/say server restarting in 30 seconds"
-sleep 30
-/minecraft/minecraft-sync.sh &>> /minecraft/minecraft-sync.log
-
-/minecraft/vanillabean.py "/stop"
-sleep 10
-/minecraft/minecraft-server.sh
--- minecraft-server.sh
+++ minecraft-server.sh
@@ -2,7 +2,7 @@
 cd /minecraft
 
 tmux new -s minecraft -d
-tmux neww -t minecraft:7 '/minecraft/minecraft-start.sh'
+tmux neww -t minecraft:7 '/minecraft/minecraft-ctl.sh start'
 tmux neww -t minecraft:6 '/minecraft/minecraft-twistd.sh'
 tmux neww -t minecraft:5 '/minecraft/minecraft-murmur.sh'
 tmux neww -t minecraft:8 '/minecraft/minecraft-notify.sh'
