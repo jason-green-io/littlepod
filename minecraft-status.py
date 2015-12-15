299a014c94c0550baab2f08bbeea98b1269b77a9
Jason Green <greener@Ari-Gold.local>
2015-12-15 21:29:18 +0000

    sed went a bit crazy there

--- minecraft-status.py
+++ minecraft-status.py
@@ -17,7 +17,7 @@ URL = config['URL']
 servername = config['name']
 webdata = config['webdata']
 
-config = '/minecraft/host/config/server.yaml'
+config = '/minecraft/host/config'
 
 servergradient = ["#FFAA00", "#F69900", "#EE8800", "#E57600", "#DD6600", "#D45500", "#CC4400", "#C33300", "#BB2100", "#B21000", "#AA0000", "#970000", "#840000", "#710000", "#5E0000", "#4B0000", "#380000", "#250000", "#120000", "#000000"]
 oldgradient = ["#FFAA00", "#F08D00", "#E27100", "#D45500", "#C63800", "#B81C00", "#AA0000", "#880000", "#660000", "#440000", "#210000", "#000000"]
