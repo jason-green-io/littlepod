acbe58dcd54b3bf46fcb165c9b1c871af02f8a33
Jason Green <greener@Ari-Gold.local>
2016-01-01 23:48:04 +0000

    well, wasn't planning on doing that today .. that was close

--- overviewer_playeractivitydb.py
+++ overviewer_playeractivitydb.py
@@ -10,6 +10,8 @@ import nbt2yaml
 import yaml
 import sqlite3
 
+
+
 with open('/minecraft/host/config/server.yaml', 'r') as configfile:
     config = yaml.load(configfile)
 
@@ -22,25 +24,18 @@ def genfilter(name):
     return filter
 
 
-timeframe = '"-7 days"'
 
-conn = sqlite3.connect(dbfile, timeout=20)
-cur = conn.cursor()
+def genpoimarkers( start, end, dim ):
+    dimdict = { "o" : "0", "n": "-1", "e": "1"}
+    conn = sqlite3.connect(dbfile, timeout=20)
+    cur = conn.cursor()
 
-cur.execute('SELECT datetime, name, x, y, z FROM location NATURAL JOIN playerUUID WHERE datetime > datetime("now", '+timeframe+') AND dim == 0')
-over = cur.fetchall()
-cur.execute('SELECT datetime, name, x, y, z FROM location NATURAL JOIN playerUUID WHERE datetime > datetime("now", '+timeframe+') AND dim == -1')
-nether = cur.fetchall()
-cur.execute('SELECT datetime, name, x, y, z FROM location NATURAL JOIN playerUUID WHERE datetime > datetime("now", '+timeframe+') AND dim == 1')
-end = cur.fetchall()
+    cur.execute('SELECT datetime, name, x, y, z FROM location NATURAL JOIN playerUUID WHERE datetime > datetime("now", "' + start + '") AND datetime < datetime("now", "' + end + '") AND dim == ' + dimdict[dim])
+    data = cur.fetchall()
 
-conn.commit()
-conn.close()
-players = {poi[1] for poi in over + nether + end}
-keys = ("datetime","name","x","y","z")
+    conn.commit()
+    conn.close()
+    players = {poi[1] for poi in data}
+    keys = ("datetime","name","x","y","z")
 
-overpoi = [dict(zip(keys, poi), id="player") for poi in over]
-netherpoi = [dict(zip(keys, poi), id="player") for poi in nether]
-endpoi = [dict(zip(keys, poi), id="player") for poi in end]
-
-markers =[dict(name="player " + player, icon="https://minotar.net/avatar/"+player+"/16", filterFunction=genfilter(player)) for player in players]
+    return ([dict(zip(keys, poi), id="player") for poi in data], [dict(checked=True, name="player " + player, icon="https://minotar.net/avatar/"+player+"/16", filterFunction=genfilter(player)) for player in players])
