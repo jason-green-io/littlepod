91a6b79fbb8515f4affcb036d6fd8340ffea18cd
Jason Green <root@greener.ca>
2016-03-18 14:53:02 +0000

    don't need this anymore

--- minecraft-ctl.sh
+++ minecraft-ctl.sh
@@ -29,7 +29,6 @@ mcstart ()
     if [[ ! -f $CLIENTVERSION.jar ]]; then
         wget -t inf https://s3.amazonaws.com/Minecraft.Download/versions/$CLIENTVERSION/$CLIENTVERSION.jar
     fi
-    cp /minecraft/host/mcdata/world/data/villages.dat.bak /minecraft/host/mcdata/world/data/villages.dat
     /usr/bin/tmux neww -t minecraft:7 "/usr/bin/java -jar minecraft_server.$VERSION.jar nogui < /minecraft/vanillabean"
 }
 
