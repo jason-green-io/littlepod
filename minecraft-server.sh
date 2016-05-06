738ba3e9c8c41e664c2f508a050c2a7f55d910be
Jason Green <root@greener.ca>
2016-05-06 15:59:53 +0000

    more changes

--- minecraft-server.sh
+++ minecraft-server.sh
@@ -4,23 +4,6 @@ echo "Creating tmux session"
 tmux new -s minecraft -d
 
 echo "Starting processes"
-pm2 start /minecraft/littlepod.json
+pm2 start /minecraft/host/config/littlepod.pm2.json
 
 
-
-#echo "Starting web sever"
-#tmux neww -t minecraft:6 '/minecraft/minecraft-twistd.sh'
-
-#echo "Starting murmur"
-#tmux neww -t minecraft:5 '/minecraft/minecraft-murmur.sh'
-
-#echo "Starting notify"
-#tmux neww -t minecraft:8 '/minecraft/minecraft-notify.sh'
-
-#echo "Starting player activity"
-#tmux neww -t minecraft:9 '/minecraft/minecraft-playeractivity.py'
-
-#echo "Starting slack integration"
-#tmux neww -t minecraft:4 '/minecraft/minecraft-slack.sh'
-#echo "Starting discord integration"
-#tmux neww -t minecraft:5 '/minecraft/minecraft-discord.sh'
