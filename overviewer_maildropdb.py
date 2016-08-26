f011405702812c8e9f606a0e5b81ebcd36d4e4f6
Jason Green <root@greener.ca>
2016-08-26 16:23:21 +0000

    there we go

--- overviewer_maildropdb.py
+++ overviewer_maildropdb.py
@@ -16,6 +16,7 @@ dbfile = config['dbfile']
 dimdict = { "nether" : "2" , "end" : "1", "over" : "0" }
 conn = sqlite3.connect(dbfile)
 cur = conn.cursor()
+
 cur.execute('select name from (select * from joins group by name order by date) where date > datetime("now", "-14 days")')
 curplayers = [player[0].lower() for player in cur.fetchall()]
 
@@ -58,42 +59,23 @@ threadDBWriter = threading.Thread(target=writeToDB)
 threadDBWriter.setDaemon(True)
 threadDBWriter.start()
 
-def maildropFilterOverworld(poi):
-    return maildropFilteruniversal( poi, "o" )
+def FilterOverworld(poi):
+    return FilterUniversal( poi, "o" )
 
 
-def maildropFilterNether(poi):
-    return maildropFilteruniversal( poi, "n" )
+def FilterNether(poi):
+    return FilterUniversal( poi, "n" )
 
 
-def maildropFilterEnd(poi):
-    return maildropFilteruniversal( poi, "e" )
+def FilterEnd(poi):
+    return FilterUniversal( poi, "e" )
 
 
-def inactiveMaildropFilterOverworld(poi):
-    return inactiveMaildropFilteruniversal( poi, "o" )
 
-
-def inactiveMaildropFilterNether(poi):
-    return inactiveMaildropFilteruniversal( poi, "n" )
-
-
-def inactiveMaildropFilterEnd(poi):
-    return inactiveMaildropFilteruniversal( poi, "e" )
-
-
-def absentMaildropFilterOverworld(poi):
-    return absentMaildropFilteruniversal( poi, "o" )
-
-
-def absentMaildropFilterNether(poi):
-    return absentMaildropFilteruniversal( poi, "n" )
-
-
-def absentMaildropFilterEnd(poi):
-    return absentMaildropFilteruniversal( poi, "e" )
-
-def inactiveMaildropFilteruniversal( poi, dim, players=oldplayers ):
+def FilterUniversal( poi, dim ):
+    global oldplayers
+    global absentplayers
+    global curplayers
     if poi['id'] in ['Chest', "minecraft:chest"]:
         if poi.has_key('CustomName'):
             rawplayer = poi['CustomName']
@@ -105,22 +87,13 @@ def inactiveMaildropFilteruniversal( poi, dim, players=oldplayers ):
     
             desc = playerdesc[1] if len(playerdesc) > 1 else ""
 
-            if player.lower() in players:
+            if player.lower() in oldplayers:
                 if not hidden:
+                    poi["icon"] = "icons/orange/temple_ruins.png"
                     return "{} maildrop\n{}".format(player, desc)
 
 
-def absentMaildropFilteruniversal( poi, dim, players=absentplayers ):
-    if poi['id'] in ['Chest', "minecraft:chest"]:
-        if poi.has_key('CustomName'):
-            rawplayer = poi['CustomName']
-            hidden = rawplayer[0] in [".", "!"]
-            inverted = rawplayer[0] == "!"
-            playerdesc = rawplayer.lstrip(".").lstrip("!").split(" ", 1)
-            player = playerdesc[0]
-    
-            desc = playerdesc[1] if len(playerdesc) > 1 else ""
-            if player.lower() in players:
+            elif player.lower() in absentplayers:
                 
                 conn = sqlite3.connect(dbfile)
                 cur = conn.cursor()
@@ -130,24 +103,13 @@ def absentMaildropFilteruniversal( poi, dim, players=absentplayers ):
                 conn.commit()
                 conn.close()
                 
-                if not (hidden or inverted):                                                                 
+                if not (hidden or inverted):
+                    poi["icon"] = "icons/orange/symbol_blank.png"
                     return "{} maildrop\n{}".format(player, desc)
                 
 
 
-def maildropFilteruniversal( poi, dim, players=curplayers ):
-
-    if poi['id'] in ['Chest', "minecraft:chest"]:
-        if poi.has_key('CustomName'):
-            rawplayer = poi['CustomName']
-            inverted = rawplayer[0] == "!"
-            hidden = rawplayer[0] in [".", "!"]
-            playerdesc = rawplayer.lstrip(".").lstrip("!").split(" ", 1)
-            player = playerdesc[0]
-    
-            desc = playerdesc[1] if len(playerdesc) > 1 else ""
-            
-            if player.lower() in players:
+            elif player.lower() in curplayers:
 
                 conn = sqlite3.connect(dbfile)
                 cur = conn.cursor()
@@ -159,18 +121,14 @@ def maildropFilteruniversal( poi, dim, players=curplayers ):
 
 
                 if not (hidden or inverted):
+                    poi["icon"] = "icons/orange/temple-2.png"
                     return "{} maildrop\n{}".format(player, desc)
    
 
 
 
-markersover = [ dict(name="maildrops (list of shame)", icon="icons/orange/temple_ruins.png", filterFunction=absentMaildropFilterOverworld, checked=False),
-               dict(name="maildrops (un-whitelisted)", icon="icons/orange/symbol_blank.png", filterFunction=inactiveMaildropFilterOverworld, checked=False),
-               dict(name="maildrops", icon="icons/orange/temple-2.png", filterFunction=maildropFilterOverworld, checked=False) ]
-markersnether = [ dict(name="maildrops (list of shame)", icon="icons/orange/temple_ruins.png", filterFunction=absentMaildropFilterNether, checked=False),
-                 dict(name="maildrops (un-whitelisted)", icon="icons/orange/symbol_blank.png", filterFunction=inactiveMaildropFilterNether, checked=False),
-                 dict(name="maildrops", icon="icons/orange/temple-2.png", filterFunction=maildropFilterNether, checked=False) ]
-markersend = [ dict(name="maildrops (list of shame)", icon="icons/ornage/temple_ruins.png", filterFunction=absentMaildropFilterEnd, checked=False),
-              dict(name="maildrops (un-whitelisted)", icon="icons/orange/symbol_blank.png", filterFunction=inactiveMaildropFilterEnd, checked=False),
-              dict(name="maildrops", icon="icons/orange/temple-2.png", filterFunction=maildropFilterEnd, checked=False) ]
+markersover = [ dict(name="maildrops", icon="icons/orange/temple_ruins.png", filterFunction=FilterOverworld, checked=False)]
+markersnether = [ dict(name="maildrops", icon="icons/orange/temple_ruins.png", filterFunction=FilterNether, checked=False)]
+markersend = [ dict(name="maildrops", icon="icons/ornage/temple_ruins.png", filterFunction=FilterEnd, checked=False)]
+
 
