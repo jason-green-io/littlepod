74d2e76624b405a5b7a7089b19e76e4590059596
Jason Green <root@greener.ca>
2017-01-01 13:47:21 +0000

    cleanup

--- minecraft-murmur.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-/usr/sbin/murmurd -ini /minecraft/host/config/murmur.ini -fg
--- minecraft-slack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /minecraft/python-rtmbot
-python -u /minecraft/python-rtmbot/rtmbot.py &> >(tee -a /minecraft/host/otherdata/logs/minecraft-slack.log) 
-
