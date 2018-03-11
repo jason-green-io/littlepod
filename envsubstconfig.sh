d3d2de644601be81f8b43ad53721d8862b1a5dbe
Jason Green <jason@green.io>
2018-03-11 14:48:44 -0400

    may this?

--- envsubstconfig.sh
+++ envsubstconfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-envsubst < /minecraft/server.properties.envsubst > /minecraft/host/mcdata/server.properties
-envsubst < /minecraft/monitrc.envsubst > /minecraft/.monitrc
+/usr/bin/envsubst < /minecraft/server.properties.envsubst > /minecraft/host/mcdata/server.properties
+/usr/bin/envsubst '$GMAILPASSWORD $GMAILUSER'< /minecraft/monitrc.envsubst > /minecraft/.monitrc
