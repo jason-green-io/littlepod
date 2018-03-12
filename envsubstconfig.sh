67773558b9a6e980af58fc2cfdb987cf99e02b51
Jason Green <jason@green.io>
2018-03-11 22:18:16 -0400

    simplification

--- envsubstconfig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/bin/envsubst < /minecraft/server.properties.envsubst > /minecraft/host/mcdata/server.properties
-/usr/bin/envsubst '$GMAILPASSWORD $GMAILUSER'< /minecraft/monitrc.envsubst > /minecraft/.monitrc
-chmod 0700 /minecraft/.monitrc
--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 #/bin/bash
-/minecraft/envsubstconfig.sh
+/usr/bin/envsubst < /minecraft/server.properties.envsubst > /minecraft/host/mcdata/server.properties
+
+/usr/bin/envsubst '$GMAILPASSWORD $GMAILUSER'< /minecraft/monitrc.envsubst > /minecraft/.monitrc
+chmod 0700 /minecraft/.monitrc
 /usr/bin/monit
+
+crontab /minecraft/crontab.txt
+
 /bin/bash
