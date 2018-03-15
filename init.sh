6b128fdcc9b4e90fc734908a9f7d3d6c8b8b1e91
Jason Green <jason@green.io>
2018-03-14 21:33:09 -0400

    let make this take care of itself

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #/bin/bash
-/usr/bin/envsubst < /minecraft/server.properties.envsubst > /minecraft/host/mcdata/server.properties
+mkdir -p $OTHERDATA
 
 /usr/bin/envsubst '$GMAILPASSWORD $GMAILUSER'< /minecraft/monitrc.envsubst > /minecraft/.monitrc
 chmod 0700 /minecraft/.monitrc
+
 /usr/bin/monit
 
 crontab /minecraft/crontab.txt
