cd3f6ce38645ceaa3d375e44534045233f28affe
Jason Green <jason@green.io>
2018-03-22 00:20:16 -0400

    oops

--- chatterbox.py
+++ chatterbox.py
@@ -68,7 +68,7 @@ def coordsmessage( reCoords, reDim ):
     for each in reCoords:
         print (each[0], each[1])
         x, z = each
-        message = "Map: {dim} {x}, {z}\n{URL}/map/{dim}/#zoom=0.02&x={x}&y={z}".format(dim=worlddict[reDim][0], x=each[0], z=each[2], URL=URL)
+        message = "Map: {dim} {x}, {z}\n{URL}/map/{dim}/#zoom=0.02&x={x}&y={z}".format(dim=worlddict[reDim][0], x=x, z=z, URL=URL)
     
     return message
 
@@ -77,7 +77,7 @@ def tellcoords( reCoords, reDim ):
     worlddict = { "o" : ["overworld", "0"], "n" : ["nether", "2"], "e" : ["end", "1"] }
     for each in reCoords:
         
-        littlepod_utils.send("/tellraw @a " + showandtellraw.tojson(serverFormat.format(servername) + "[Map: _{dim} {x}, {z}_|{URL}/map/{dim}/#zoom=0.02&x={x}&y={z}]".format(dim=worlddict[reDim][0], x=each[0], z=each[2], URL=URL)))
+        littlepod_utils.send("/tellraw @a " + showandtellraw.tojson(serverFormat.format(servername) + "[Map: _{dim} {x}, {z}_|{URL}/map/{dim}/#zoom=0.02&x={x}&y={z}]".format(dim=worlddict[reDim][0], x=x, z=z, URL=URL)))
 
 
 
