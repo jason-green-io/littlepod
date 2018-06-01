9cb5486b28c57c04a800df7374b84ee6f04290e3
Jason Green <jason@green.io>
2018-05-31 23:08:35 -0400

    more changes

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 mkdir -p $OTHERDATA/logs
 echo $MCVERSION > /minecraft/mcversion
-/usr/bin/envsubst '$GMAILPASSWORD $GMAILUSER'< /minecraft/monitrc.envsubst > /minecraft/.monitrc
+/usr/bin/envsubst '$GMAILPASSWORD $GMAILUSER $MCVERSION' < /minecraft/monitrc.envsubst > /minecraft/.monitrc
 chmod 0700 /minecraft/.monitrc
 
 /usr/bin/monit
